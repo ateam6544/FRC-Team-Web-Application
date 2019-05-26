ae96484e3c7a98f94505327c375a8c4d9724e123
Adam Tronchin <atronchin34@gmail.com>
2019-05-26 14:47:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FRC-Team-Web-App
---
+++ public/js/mongoDB.js
@@ -0,0 +1,5 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost/frcTeamsApp';
+
+MongoClient.connect(url, function(err, db) {
+}); 
